339aeec2033631831f68706a1abcdbcf53027df2
Pedro Nogueira <pedro.nogueira.barboza@gmail.com>
2024-07-02 19:24:16 -0300

    feat: adicionei parâmetros melhores.

--- src/routes/movie.routes.js
+++ src/routes/movie.routes.js
@@ -2,7 +2,17 @@ import express from "express";
 import * as movieController from "../controllers/movie.controller.js"; // importa todos os métodos do controller
 const router = express.Router();
 
-router.get("/page/:page", movieController.getMoviesApi);
-router.get("/search/page/:searchPage", movieController.getMoviesBySearch);
+import { jwtAuthMiddlewareCookie } from "../middlewares/auth.js";
+
+router.get(
+  "/page/:page",
+  jwtAuthMiddlewareCookie,
+  movieController.getMoviesApi
+);
+router.get(
+  "/search/page/:searchPage",
+  jwtAuthMiddlewareCookie,
+  movieController.getMoviesBySearch
+);
 
 export default router;
