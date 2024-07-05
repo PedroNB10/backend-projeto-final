d639f137b6c4cb96be07791500a39cdb0b356f20
Pedro Nogueira <pedro.nogueira.barboza@gmail.com>
2024-07-05 13:55:34 -0300

    refact: refatorando código colocando status 500 erro no servidor.

--- src/controllers/review.controller.js
+++ src/controllers/review.controller.js
@@ -1,12 +1,6 @@
 import fs from "node:fs";
 
-import {
-  getUsersRegistered,
-  getMovies,
-  getReviews,
-  __dirname,
-} from "../connection.db.js";
-import { User } from "../models/user.js";
+import { getUsersRegistered, getReviews, __dirname } from "../connection.db.js";
 import { Movie } from "../models/movie.js";
 import { Review } from "../models/review.js";
 import * as userController from "../controllers/user.controller.js"; // importa todos os métodos do controller
@@ -17,8 +11,6 @@ const reviewsDatabasePath = path.join(__dirname, "..", "db", "reviews.json");
 const usersDatabasePath = path.join(__dirname, "..", "db", "users.json");
 
 export async function createReview(req, res) {
-  const reviews = getReviews();
-  const users = getUsersRegistered();
   const {
     content,
     rating,
@@ -30,124 +22,145 @@ export async function createReview(req, res) {
     poster_path,
   } = req.body;
 
-  const user = getUsersRegistered().find((user) => user.id === userId);
-
-  if (!user) {
-    return res.status(400).send("Usuário não encontrado.");
+  if (
+    !content ||
+    !rating ||
+    !userId ||
+    !movieId ||
+    !title ||
+    !overview ||
+    !release_date ||
+    !poster_path
+  ) {
+    return res.status(400).send("Preencha todos os campos obrigatórios.");
   }
 
-  const id = reviews.length + 1;
+  try {
+    const reviews = getReviews();
+    const users = getUsersRegistered();
 
-  const review = new Review(
-    id,
-    content,
-    rating,
-    userId,
-    movieId,
-    title,
-    overview,
-    release_date,
-    poster_path
-  );
+    const user = getUsersRegistered().find((user) => user.id === userId);
 
-  const movie = new Movie(movieId, title, overview, release_date, poster_path);
-  // cria o filme no banco de dados de filmes avaliados
-  movieController.createMovie(movie);
-
-  // console.log(user.reviews);
-
-  const userReviews = user.reviews;
-  const isThereAReview = userReviews.find(
-    (review) => Number(review.movie.id) === Number(movieId)
-  );
-
-  if (isThereAReview) {
-    isThereAReview.content = content;
-    isThereAReview.rating = rating;
-    isThereAReview.date = new Date();
-    for (let i = 0; i < reviews.length; i++) {
-      if (reviews[i].id === isThereAReview.id) {
-        reviews[i] = isThereAReview;
-
-        for (let i = 0; i < user.reviews.length; i++) {
-          if (user.reviews[i].id === isThereAReview.id) {
-            user.reviews[i] = isThereAReview;
-
-            const userIndex = users.findIndex((user) => user.id === userId);
-
-            if (userIndex === -1) {
-              return res.status(404).send("Usuário não encontrado.");
-            }
-
-            users[userIndex] = user;
-            fs.writeFileSync(
-              reviewsDatabasePath,
-              JSON.stringify(reviews, null, 2)
-            );
-            fs.writeFileSync(usersDatabasePath, JSON.stringify(users, null, 2));
-          }
-        }
-
-        res
-          .status(200)
-          .send({ id: isThereAReview.id, message: "Avaliação atualizada!" });
-      }
+    if (!user) {
+      return res.status(400).send("Usuário não encontrado.");
     }
-  } else {
-    reviews.push(review);
-    userController.addLastReview(review);
-    fs.writeFileSync(reviewsDatabasePath, JSON.stringify(reviews, null, 2));
 
-    res
-      .status(201)
-      .send({ id: review.id, message: "Avaliação criada com sucesso!" });
+    const id = reviews.length + 1;
+
+    const review = new Review(
+      id,
+      content,
+      rating,
+      userId,
+      movieId,
+      title,
+      overview,
+      release_date,
+      poster_path
+    );
+
+    const movie = new Movie(
+      movieId,
+      title,
+      overview,
+      release_date,
+      poster_path
+    );
+    // cria o filme no banco de dados de filmes avaliados
+    movieController.createMovie(movie);
+
+    // console.log(user.reviews);
+
+    const userReviews = user.reviews;
+    const isThereAReview = userReviews.find(
+      (review) => Number(review.movie.id) === Number(movieId)
+    );
+
+    if (isThereAReview) {
+      isThereAReview.content = content;
+      isThereAReview.rating = rating;
+      isThereAReview.date = new Date();
+      for (let i = 0; i < reviews.length; i++) {
+        if (reviews[i].id === isThereAReview.id) {
+          reviews[i] = isThereAReview;
+
+          for (let i = 0; i < user.reviews.length; i++) {
+            if (user.reviews[i].id === isThereAReview.id) {
+              user.reviews[i] = isThereAReview;
+
+              const userIndex = users.findIndex((user) => user.id === userId);
+
+              if (userIndex === -1) {
+                return res.status(404).send("Usuário não encontrado.");
+              }
+
+              users[userIndex] = user;
+              fs.writeFileSync(
+                reviewsDatabasePath,
+                JSON.stringify(reviews, null, 2)
+              );
+              fs.writeFileSync(
+                usersDatabasePath,
+                JSON.stringify(users, null, 2)
+              );
+            }
+          }
+
+          res
+            .status(200)
+            .send({ id: isThereAReview.id, message: "Avaliação atualizada!" });
+        }
+      }
+    } else {
+      reviews.push(review);
+      userController.addLastReview(review);
+      fs.writeFileSync(reviewsDatabasePath, JSON.stringify(reviews, null, 2));
+
+      res
+        .status(201)
+        .send({ id: review.id, message: "Avaliação criada com sucesso!" });
+    }
+  } catch (error) {
+    console.error("Erro ao criar review:", error);
+    return res.status(500).json({ message: "Erro ao criar review" });
   }
 }
 
 export async function deleteReview(req, res) {
-  const reviews = getReviews();
   let { reviewId } = req.params;
 
-  reviewId = parseInt(reviewId);
-
-  const reviewIndex = reviews.findIndex((review) => review.id === reviewId);
-
-  if (reviewIndex === -1) {
-    return res.status(404).send("Review não encontrada.");
+  if (!reviewId) {
+    return res.status(400).send("ID da review é obrigatório.");
   }
 
-  const review = reviews[reviewIndex];
-  const user = getUsersRegistered().find((user) => user.id === review.userId);
+  try {
+    reviewId = parseInt(reviewId);
 
-  if (!user) {
-    return res.status(404).send("Usuário não encontrado.");
+    const reviews = getReviews();
+    const reviewIndex = reviews.findIndex((review) => review.id === reviewId);
+
+    if (reviewIndex === -1) {
+      return res.status(404).send("Review não encontrada.");
+    }
+
+    const review = reviews[reviewIndex];
+    const user = getUsersRegistered().find((user) => user.id === review.userId);
+
+    if (!user) {
+      return res.status(404).send("Usuário não encontrado.");
+    }
+
+    userController.removeReview(review);
+
+    reviews.splice(reviewIndex, 1);
+
+    fs.writeFileSync(reviewsDatabasePath, JSON.stringify(reviews, null, 2));
+
+    return res.status(200).send("Review removida com sucesso.");
+  } catch (error) {
+    console.error("Erro ao deletar review:", error);
+    return res.status(500).json({ message: "Erro ao deletar review" });
   }
-
-  userController.removeReview(review);
-
-  reviews.splice(reviewIndex, 1);
-
-  fs.writeFileSync(reviewsDatabasePath, JSON.stringify(reviews, null, 2));
-
-  res.status(200).send("Review removida com sucesso.");
-}
-
-export async function getReviewsByMovieId(req, res) {
-  const { movie_id } = req.params;
-  const reviews = getReviews();
-  const reviewsByMovieId = reviews.filter(
-    (review) => review.movie_id === movie_id
-  );
-
-  res.send(reviewsByMovieId);
-}
-
-export async function getReviewsByUserId(req, res) {
-  const { userId } = req.params;
-  const reviews = getReviews();
-  const reviewsByUserId = reviews.filter((review) => review.userId === userId);
-
-  res.send(reviewsByUserId);
 }
 
 // atualizar review - usar id do usuário e id da avaliação
@@ -155,33 +168,44 @@ export async function getReviewsByUserId(req, res) {
 
 export async function updateReview(req, res) {
   // Obtém o ID do usuário e da review pelos parâmetros da URL
-  const userId = req.params.userId; 
+  const userId = req.params.userId;
   const reviewId = parseInt(req.params.reviewId); // Converte para número
-  const { content, rating } = req.body  ;// Obtém o novo conteúdo e avaliação da review
+  const { content, rating } = req.body; // Obtém o novo conteúdo e avaliação da review
+
+  if (!content || !rating || !userId || !reviewId) {
+    return res
+      .status(400)
+      .json({ message: "Preencha todos os campos obrigatórios." });
+  }
 
   try {
     // Obtém todas as reviews do banco
     const reviews = getReviews();
 
     // Encontra a review com base no reviewId e userId
-    const reviewIndex = reviews.findIndex(review => review.id === reviewId && review.userId === userId);
+    const reviewIndex = reviews.findIndex(
+      (review) => review.id === reviewId && review.userId === userId
+    );
 
     if (reviewIndex === -1) {
-      return res.status(404).json({ message: 'Review não encontrada' });
+      return res.status(404).json({ message: "Review não encontrada" });
     }
 
     // Atualiza os campos da review (conteúdo, avaliação e data)
-    reviews[reviewIndex].content = content; 
-    reviews[reviewIndex].rating = rating; 
+    reviews[reviewIndex].content = content;
+    reviews[reviewIndex].rating = rating;
     reviews[reviewIndex].date = new Date();
 
     // Salva as reviews de volta no banco (arquivo)
     fs.writeFileSync(reviewsDatabasePath, JSON.stringify(reviews, null, 2));
 
     // Retorna uma resposta de sucesso
-    return res.status(200).json({ message: 'Review atualizada com sucesso', review: reviews[reviewIndex] });
+    return res.status(200).json({
+      message: "Review atualizada com sucesso",
+      review: reviews[reviewIndex],
+    });
   } catch (error) {
-    console.error('Erro ao atualizar review:', error);
-    return res.status(500).json({ message: 'Erro ao atualizar review' });
+    console.error("Erro ao atualizar review:", error);
+    return res.status(500).json({ message: "Erro ao atualizar review" });
   }
 }
