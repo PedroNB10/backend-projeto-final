8717fb52ba6fcbdd548ad30eeb3ccd6b1da6a147
Pedro Nogueira <pedro.nogueira.barboza@gmail.com>
2024-07-05 14:02:31 -0300

    refact: deixar as funções auxiliares posterior as funções de execução do banco.

--- src/controllers/movie.controller.js
+++ src/controllers/movie.controller.js
@@ -7,25 +7,6 @@ const moviesDatabasePath = path.join(__dirname, "..", "db", "movies.json");
 import "../config.js"; // importando as variáveis de ambiente
 import axios from "axios";
 
-export async function createMovie(movie) {
-  const movies = getMovies();
-  const { id, title, overview, release_date, poster_path } = movie;
-
-  const movieAlreadyExists = movies.find((movie) => movie.id === id);
-  if (movieAlreadyExists) {
-    return;
-  }
-
-  const newMovie = new Movie(id, title, overview, release_date, poster_path);
-  movies.push(newMovie);
-  fs.writeFileSync(moviesDatabasePath, JSON.stringify(movies, null, 2));
-}
-
-export async function getMovieById(movieId) {
-  const movies = getMovies();
-  return movies.find((movie) => movie.id === movieId);
-}
-
 export async function getMoviesApi(req, res) {
   const { page } = req.params;
 
@@ -67,4 +48,23 @@ export async function getMoviesBySearch(req, res) {
       console.error(error);
       res.status(500).json({ message: "Erro ao buscar filmes search" });
     });
+}
+
+export async function createMovie(movie) {
+  const movies = getMovies();
+  const { id, title, overview, release_date, poster_path } = movie;
+
+  const movieAlreadyExists = movies.find((movie) => movie.id === id);
+  if (movieAlreadyExists) {
+    return;
+  }
+
+  const newMovie = new Movie(id, title, overview, release_date, poster_path);
+  movies.push(newMovie);
+  fs.writeFileSync(moviesDatabasePath, JSON.stringify(movies, null, 2));
+}
+
+export async function getMovieById(movieId) {
+  const movies = getMovies();
+  return movies.find((movie) => movie.id === movieId);
 }
